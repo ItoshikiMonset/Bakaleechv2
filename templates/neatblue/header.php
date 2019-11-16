d7d9eace934a90fa019739d8f72611f069b0b082
ItoshikiMonset <46911980+ItoshikiMonset@users.noreply.github.com>
2019-11-15 23:33:30 -0500

    Update header.php

--- templates/neatblue/header.php
+++ templates/neatblue/header.php
@@ -31,4 +31,4 @@ if ($GLOBALS['options']['flist_sort']) { echo '<script src="classes/sorttable.js
 <script type="text/javascript">function toggle(b){var a=document.getElementById(b);if(a.style.display=="none"){a.style.display="block"}else{a.style.display="none"}};</script>
 </head>
 <body>
-<header><img src="https://raw.githubusercontent.com/ItoshikiMonset/Bakaleechv2/master/templates/neatblue/images/header2.jpg?token=ALF5D3DL2VEER2UEM6LVMH253CWIG" height="220" width="700" alt="rapidleech neatblue by Itoshiki"></header><br />
+<header><img src="https://i.ibb.co/Pr6QsrS/header2.jpg" height="220" width="650" alt="rapidleech neatblue by Itoshiki"></header><br />
